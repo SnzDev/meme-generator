284ffd45317f508388bd2aae59d106c9768bd964
SnzDev <tms.working@gmail.com>
2022-09-27 22:55:31 -0300

    feat(index.js): add meme generator

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello meme-generator');
+import memeMaker from "@erickwendel/meme-maker";
+
+
+const options = {
+  image: "./img/sorry.jpg",
+  outfile: './img/spiderman-meme.jpg',
+  topText: 'TODAY IM',
+  bottomText: 'AN ASS',
+}
+
+
+memeMaker(options).then(_ => {
+  console.log('Image saved: ' + options.outfile);
+}).catch(error => console.log(error));
